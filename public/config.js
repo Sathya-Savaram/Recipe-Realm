fe598735e46e4b9a32b293ed5b8ce88d4411a42a
Sathya-Savaram <ssavaram@gitam.in>
2024-07-08 18:51:47 +0530

    First Commit

---
+++ public/config.js
@@ -0,0 +1,22 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBV4uXfT7aA48HElTMRxXeEkKpK5m5rU7M",
+  authDomain: "recipe-realm-83043.firebaseapp.com",
+  projectId: "recipe-realm-83043",
+  storageBucket: "recipe-realm-83043.appspot.com",
+  messagingSenderId: "177441287999",
+  appId: "1:177441287999:web:b2ea72e8aa55f923df6d8a",
+  measurementId: "G-7S2VKE81BS"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
